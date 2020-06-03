1ed2d4db95ec5664449ce34dfbc5d6467f70b489
rafhaelbarabas <rafhael_ab@hotmail.com>
2020-06-03 14:41:32 -0300

    create point collector page and some features

---
+++ scripts/create-point.js
@@ -0,0 +1,47 @@
+// function handleOptionsSelectors(url, element){
+//   fetch(url)
+//   .then(res => res.json())
+//   .then( data => {
+//     for(item of data){
+//       element.innerHTML += `<option value="${item.id}">${item.nome}</option>`
+//     }
+//   })
+// }
+
+function populateUFs() {
+  let ufSelect = document.querySelector("select[name=uf]")  
+
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
+    .then(res => res.json())
+    .then(states => {
+      for (state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+      }
+    })
+}
+
+function getCities(event) {
+
+  let citySelect = document.querySelector("select[name=city]")
+
+  // Limpa o valor do select antes de inserir os novos
+  citySelect.innerHTML = `<option value="">Selecione a cidade</option>"`
+
+  let uf = event.target.value
+
+  fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios?orderBy=nome`)
+    .then(res => res.json())
+    .then(cities => {
+      for (city of cities) {
+        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      }
+      citySelect.disabled = false
+    })
+
+    let stateInput = document.querySelector("input[name=state]")
+    stateInput.value = event.target.options[event.target.selectedIndex].text
+}
+
+populateUFs()
+
+document.querySelector("select[name=uf").addEventListener("change", getCities)
